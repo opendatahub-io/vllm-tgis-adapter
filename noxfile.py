4aebbae09285d2fe719d35cae404b46ce92167bd
Daniele Trifirò <dtrifiro@redhat.com>
2024-05-06 11:42:20 +0200

    add github actions, nox configuration, pre-commit configuration

---
+++ noxfile.py
@@ -0,0 +1,61 @@
+from pathlib import Path
+
+import nox
+
+nox.options.reuse_existing_virtualenvs = True
+nox.options.sessions = "lint", "tests"
+locations = "src", "tests"
+
+versions = ["3.9", "3.10", "3.11", "3.12"]
+
+
+@nox.session(python=versions)
+def tests(session: nox.Session) -> None:
+    session.install(".[tests]")
+    session.run(
+        "pytest",
+        "--cov",
+        "--cov-config=pyproject.toml",
+        *session.posargs,
+        env={"COVERAGE_FILE": f".coverage.{session.python}"},
+    )
+
+
+@nox.session(python=versions)
+def lint(session: nox.Session) -> None:
+    session.install("pre-commit")
+    session.install("-e", ".[dev]")
+
+    args = *(session.posargs or ("--show-diff-on-failure",)), "--all-files"
+    session.run("pre-commit", "run", *args)
+    session.run("python", "-m", "mypy")
+
+
+@nox.session(python=versions)
+def safety(session: nox.Session) -> None:
+    """Scan dependencies for insecure packages."""
+    session.install(".[dev]")
+    session.install("safety")
+    session.run("safety", "check", "--full-report")
+
+
+@nox.session(python=versions)
+def build(session: nox.Session) -> None:
+    session.install("build", "setuptools", "twine")
+    session.run("python", "-m", "build")
+    dists = Path("dist").glob("*")
+    session.run("twine", "check", *dists, silent=True)
+
+
+@nox.session(python=versions)
+def dev(session: nox.Session) -> None:
+    """Set up a python development environment for the project."""
+    args = session.posargs or ("venv",)
+    venv_dir = Path(args[0])
+
+    session.log(f"Setting up virtual environment in {venv_dir}")
+    session.install("virtualenv")
+    session.run("virtualenv", venv_dir, silent=True)
+
+    python = venv_dir / "bin/python"
+    session.run(python, "-m", "pip", "install", "-e", ".[dev]", external=True)
