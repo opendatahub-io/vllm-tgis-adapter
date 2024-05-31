406c07e46f424bc7109973364c6eec585515d053
Daniele Trifirò <dtrifiro@redhat.com>
2024-05-31 16:50:47 +0000

    properly handle grpc server interruption

--- src/vllm_tgis_adapter/__main__.py
+++ src/vllm_tgis_adapter/__main__.py
@@ -36,7 +36,7 @@ async def grpc_server(
     try:
         while True:
             await asyncio.sleep(60)
-    except KeyboardInterrupt:
+    except asyncio.CancelledError:
         logger.info("Gracefully stopping gRPC server")
         await server.stop(30)  # TODO configurable grace
         await server.wait_for_termination()
