58093c3663269c4232231e647eb65371f4f1d7d4
Daniele Trifirò <dtrifiro@redhat.com>
2024-07-02 19:22:44 +0200

    tests: add tests using real engine instance for both servers

--- tests/test_grpc_server.py
+++ tests/test_grpc_server.py
@@ -1,6 +1,50 @@
 import pytest
 
+from .utils import GrpcClient
 
-@pytest.mark.usefixtures("_grpc_server")
-def test_startup():
-    """Test that the grpc_server fixture starts up properly."""
+
+@pytest.fixture()
+def grpc_client(grpc_server_thread_port, _grpc_server):
+    """Return a grpc client connected to the grpc server."""
+    with GrpcClient(
+        host="localhost",
+        port=grpc_server_thread_port,
+        insecure=True,
+    ) as client:
+        yield client
+
+
+def test_generation_request(grpc_client, grpc_server_thread_port):
+    response = grpc_client.make_request(
+        "The answer to life the universe and everything is "
+    )
+
+    assert response.text
+    assert response.generated_token_count
+    assert response.stop_reason is not None
+
+
+def test_generation_request_stream(grpc_client, grpc_server_thread_port):
+    streaming_response = grpc_client.make_request_stream(
+        "The answer to life the universe and everything is ",
+    )
+
+    text_chunks: list[str] = [chunk.text for chunk in streaming_response]
+
+    assert text_chunks
+    assert len(text_chunks) == 11
+    assert "".join(text_chunks)
+
+
+def test_batched_generation_request(grpc_client, grpc_server_thread_port):
+    responses = list(
+        grpc_client.make_request(
+            [
+                "The answer to life the universe and everything is ",
+                "Medicinal herbs ",
+            ],
+        )
+    )
+
+    assert len(responses) == 2
+    assert all(response.text for response in responses)
--- tests/test_http_server.py
+++ tests/test_http_server.py
@@ -1,9 +1,29 @@
-import pytest
 import requests
 
 
-@pytest.mark.usefixtures("_http_server")
-def test_startup(http_server_url):
+def test_startup(http_server_url, _http_server):
     """Test that the http_server fixture starts up properly."""
     requests.get(f"{http_server_url}/health").raise_for_status()
-    # requests.get(f"{http_server_url}/v1/models").raise_for_status()
+
+
+def test_completions(http_server_url, _http_server):
+    response = requests.get(f"{http_server_url}/v1/models")
+    response.raise_for_status()
+
+    data = response.json()["data"]
+    model_id = data[0]["id"]
+
+    response = requests.post(
+        f"{http_server_url}/v1/completions",
+        json={
+            "prompt": "The answer tho life the universe and everything is ",
+            "model": model_id,
+        },
+    )
+    response.raise_for_status()
+
+    completion = response.json()
+
+    generated_text = completion["choices"][0]["text"]
+
+    assert generated_text
