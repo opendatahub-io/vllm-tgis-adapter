a7d89dcf445feceec0546f3befa0ce94073694b9
Joe Runde <Joseph.Runde@ibm.com>
2024-08-09 11:23:02 -0600

    :bug: add backwards compatibility
    Signed-off-by: Joe Runde <Joseph.Runde@ibm.com>

--- src/vllm_tgis_adapter/http.py
+++ src/vllm_tgis_adapter/http.py
@@ -29,16 +29,25 @@ async def run_http_server(
 
     app = await init_app(engine, args)  # type: ignore[arg-type]
 
-    await serve_http(
-        app,
-        engine,
-        host=args.host,
-        port=args.port,
-        log_level=args.uvicorn_log_level,
-        timeout_keep_alive=TIMEOUT_KEEP_ALIVE,
-        ssl_keyfile=args.ssl_keyfile,
-        ssl_certfile=args.ssl_certfile,
-        ssl_ca_certs=args.ssl_ca_certs,
-        ssl_cert_reqs=args.ssl_cert_reqs,
-        **uvicorn_kwargs,
-    )
+    serve_kwargs = {
+        "host": args.host,
+        "port": args.port,
+        "log_level": args.uvicorn_log_level,
+        "timeout_keep_alive": TIMEOUT_KEEP_ALIVE,
+        "ssl_keyfile": args.ssl_keyfile,
+        "ssl_certfile": args.ssl_certfile,
+        "ssl_ca_certs": args.ssl_ca_certs,
+        "ssl_cert_reqs": args.ssl_cert_reqs,
+    }
+    serve_kwargs.update(uvicorn_kwargs)
+
+    try:
+        shutdown_coro = await serve_http(app, engine, **serve_kwargs)
+    except TypeError:
+        # vllm 0.5.4 backwards compatibility
+        # HTTP server will not shut itself down when the engine dies
+        shutdown_coro = await serve_http(app, **serve_kwargs)
+
+    # launcher.serve_http returns a shutdown coroutine to await
+    # (The double await is intentional)
+    await shutdown_coro
