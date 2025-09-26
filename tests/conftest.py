b0118069edc7692110fe72472bc9f3c6b3870da0
Daniele Trifirò <dtrifiro@redhat.com>
2025-09-26 17:12:49 +0200

    tests: remove useless server fixture args

--- tests/conftest.py
+++ tests/conftest.py
@@ -89,7 +89,6 @@ def args(  # noqa: PLR0913
             "__main__.py",
             f"--grpc-port={grpc_server_port}",
             f"--port={http_server_port}",
-            "--dtype=float32",
             "--max-model-len=512",
             *extra_args,
         ],
